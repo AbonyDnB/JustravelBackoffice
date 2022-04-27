1c2fcbfd352f41c426f28c31fbf67e7c9fa1a94a
ayoub <ayoub.dalil@homerti.com>
2022-04-27 11:40:22 +0200

    Services stepper implement

--- src/app/pages/services/create-service/create-service.component.ts
+++ src/app/pages/services/create-service/create-service.component.ts
@@ -6,6 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-service.component.scss']
 })
 export class CreateServiceComponent implements OnInit {
+  isActiveComida: string[] = ['activo', 'inactivo'];
+  requiereReserva: string[] = ['no', 'por telefono'];
+  establishments: string[] = ['Dakota Tex Mex', 'Can Pescador'];
+  // control de textareas descripciones
+  textES: boolean = true;
+  textEN: boolean = false;
+  textDE: boolean = false;
+
+  // --------------------- MOSTRAR TEXTAREA DE DESCRIPCIONES SEGÚN EL BTN CLICADO ---------------------
+
+  textESFunction() {
+    this.textES = true;
+    this.textEN = false;
+    this.textDE = false
+  }
+
+  textENFunction() {
+    this.textEN = true;
+    this.textES = false;
+    this.textDE = false
+  }
+
+  textDEFunction() {
+    this.textDE = true;
+    this.textEN = false;
+    this.textES = false
+  }
 
   constructor() { }
 
